b969628f9a76aa7a2fe4b5b47fa787fcbda3ab43
Brian <xan.brian@gmail.com>
2022-09-17 02:10:14 +0800

    up to jdk17

--- src/test/java/com/insight/utils/AppTest.java
+++ src/test/java/com/insight/utils/AppTest.java
@@ -1,20 +1,14 @@
 package com.insight.utils;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
      */
-    @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
     }
 }
